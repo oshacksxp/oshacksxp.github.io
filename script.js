9d5c2a49a842efe1345b2aff590e3a31fc90f70d
Aedin Hunter A. Clay <aedin.clay@obf.ateneo.edu>
2021-04-28 19:49:33 +0800

    Redesign of Website I

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-/* When the user scrolls down, hide the webiste header. When the user scrolls up, show it */
-/* Script taken from https://www.w3schools.com/cssref/pr_background-image.asp */ 
 
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("website-header").style.top = "0";
-  } else {
-    document.getElementById("website-header").style.top = "-15vh";
-  }
-  prevScrollpos = currentScrollPos;
-}
