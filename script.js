bc3f710a14a335174b1615893e06d0e21e8c7719
Aedin Hunter A. Clay <aedin.clay@obf.ateneo.edu>
2021-05-06 16:57:46 +0800

    added FAQ and fixed mentor button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* When the user scrolls down, hide the webiste header. When the user scrolls up, show it */
 /* Script taken from https://www.w3schools.com/cssref/pr_background-image.asp */ 
-
+/*
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   currentScrollPos = window.pageYOffset;
@@ -10,4 +10,23 @@ window.onscroll = function() {
     document.getElementById("website-header").style.top = "-15vh";
   }
   prevScrollpos = currentScrollPos;
+}
+*/
+
+window.onload = function(){
+  let accordions = document.getElementsByClassName("accordion");
+  for (let i = 0; i < accordions.length; i++) {
+    /*Add Onclick Event*/
+    accordions[i].addEventListener("click", function() {
+      /*Set Active*/
+      this.classList.toggle("active");
+      /*Toggle Panel Onclick*/
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) { /*if max height is greater than 0 //  panel is visible */
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 }
